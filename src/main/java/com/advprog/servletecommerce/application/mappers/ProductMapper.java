b6f8029315e84b0fffb90b25cf3cfc601c574c13
seif hatem <seif.hatem138@gmail.com>
2026-05-10 01:53:20 +0300

    feat: add product mapper

---
+++ src/main/java/com/advprog/servletecommerce/application/mappers/ProductMapper.java
@@ -0,0 +1,47 @@
+package com.advprog.servletecommerce.application.mappers;
+
+import java.util.List;
+
+import com.advprog.servletecommerce.domain.dto.ProductDetailsDto;
+import com.advprog.servletecommerce.domain.dto.ProductDto;
+import com.advprog.servletecommerce.domain.dto.RegisterRequestDto;
+import com.advprog.servletecommerce.domain.entities.Product;
+import com.advprog.servletecommerce.domain.entities.User;
+import com.advprog.servletecommerce.domain.enums.Role;
+
+public class ProductMapper {
+    public static Product toEntity(ProductDto productDto) {
+        return Product.builder()
+                .id(productDto.id())
+                .name(productDto.name())
+                .price(productDto.price())
+                .stockQuantity(productDto.stockQuantity())
+                .build();
+    }
+    public static ProductDto toDto(Product product){
+        return ProductDto.builder()
+                         .id(product.getId())
+                         .name(product.getName())
+                         .price(product.getPrice())
+                         .stockQuantity(product.getStockQuantity())
+                         .build();
+    }
+    public static Product toEntity(ProductDetailsDto productDetailsDto) {
+        return Product.builder()
+                      .id(productDetailsDto.id())
+                      .name(productDetailsDto.name())
+                      .price(productDetailsDto.price())
+                      .description(productDetailsDto.description())
+                      .stockQuantity(productDetailsDto.stockQuantity())
+                      .build();
+    }
+    public static ProductDetailsDto toDetailsDto(Product product){
+        return ProductDetailsDto.builder()
+                         .id(product.getId())
+                         .name(product.getName())
+                         .price(product.getPrice())
+                         .description(product.getDescription())
+                         .stockQuantity(product.getStockQuantity())
+                         .build();
+    }
+}
